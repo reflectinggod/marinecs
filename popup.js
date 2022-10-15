587dcf12a21ae47e5c6291eeeb080d78932199a7
rbobot <rplant2020@gmail.com>
2022-10-15 12:19:12 +0300

    Add Copy button

--- popup.js
+++ popup.js
@@ -1,9 +1,24 @@
-const button = document.getElementById("common-btn");
-button.onclick = function () {
-    const contentToCopy = document.getElementById("common-txt").innerText;
-    navigator.clipboard.writeText(contentToCopy).then(function() {
-        console.log('Copied to clipboard.');
-    }, function(err) {
-        console.error('Unable to copy ', err);
-    });
+const p110btn = document.getElementById("p110-btn");
+p110btn.onclick = function () {
+  const contentToCopy = document.getElementById("p110").innerText;
+  navigator.clipboard.writeText(contentToCopy).then(
+    function () {
+      console.log("Copied to clipboard.");
+    },
+    function (err) {
+      console.error("Unable to copy ", err);
+    }
+  );
+};
+const ruspriorbtn = document.getElementById("rusprior-btn");
+ruspriorbtn.onclick = function () {
+  const contentToCopy = document.getElementById("rusprior").innerText;
+  navigator.clipboard.writeText(contentToCopy).then(
+    function () {
+      console.log("Copied to clipboard.");
+    },
+    function (err) {
+      console.error("Unable to copy ", err);
+    }
+  );
 };
